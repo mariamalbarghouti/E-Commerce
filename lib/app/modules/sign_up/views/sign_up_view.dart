66f3d752bbf502fdc4fe0b46f7292b4e16edda22
Mariam Albarghouti <mariamalbarghouti@gmail.com>
2022-01-04 10:01:54 +0200

    first commit

--- lib/app/modules/sign_up/views/sign_up_view.dart
+++ lib/app/modules/sign_up/views/sign_up_view.dart
@@ -13,39 +13,7 @@ class SignUpView extends GetView<SignUpController> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child:  Form(
-            key: controller.registrationKey,
-            child: Column(
-              children: [
-                // Email
-                TextFormField(
-                  controller: controller.emailEditionController.value,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  decoration: const InputDecoration(
-                    labelText: "Email",
-                  ),
-                  validator: (_) => controller.emailValidation(),
-                  textInputAction: TextInputAction.next,
-                ),
-                // Password
-                TextFormField(
-                  controller: controller.passwordEditionController.value,
-                  decoration: const InputDecoration(
-                    labelText: "Password",
-                  ),
-                  textInputAction: TextInputAction.done,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (_) => controller.passwordValidation(),
-                ),
-                // Sign In Button
-                ElevatedButton(
-                  onPressed: () async=>await  controller.signIn(),
-                  child: const Text("Sign In"),
-                ),
-              ],
-            ),
-          ),
-        ),
+        
       ),
     );
   }
