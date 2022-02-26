e7430bb58b76082d51581816c3e80fec2f3e98d2
Mariam Albarghouti <mariamalbarghouti@gmail.com>
2022-02-26 20:04:10 +0200

    modification

--- lib/app/core/domain/failures/server_failures/curd_server_error.dart
+++ lib/app/core/domain/failures/server_failures/curd_server_error.dart
@@ -5,17 +5,20 @@ part 'curd_server_error.freezed.dart';
 
 // Registration Server Failures
 @freezed
-abstract class CURDOperationsServerFailures with _$CURDOperationsServerFailures {
+abstract class CURDOperationsServerFailures
+    with _$CURDOperationsServerFailures {
   /// Server Error
   const factory CURDOperationsServerFailures.serverError(
       {@Default("Server Error") String msg}) = _ServerError;
-  /// Permission Denied 
+
+  /// Permission Denied
   const factory CURDOperationsServerFailures.permissionsDenied(
       {@Default("Permission Denied") String msg}) = _PermissionsDenied;
-     /// Unexpected Error
+
+  /// Unexpected Error
   const factory CURDOperationsServerFailures.unexpectedError(
       {@Default("Unexpected Error") String msg}) = _UnexpectedError;
-     // No More Data
+  // No More Data If user need to Fetch more
   const factory CURDOperationsServerFailures.noMoreData(
       {@Default("There Is No More Data") String msg}) = _NoMoreData;
 }
